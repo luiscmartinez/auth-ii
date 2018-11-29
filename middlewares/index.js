894cee023c06dbd4b212bc577d8e54a825cbfed1
luiscmartinez <mrluismartinezzz@gmail.com>
2018-11-29 03:45:31 -0800

    server uses rate limiter

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,10 +2,16 @@ const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
 const helmet = require('helmet')
+const rateLimit = require('express-rate-limit')
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+})
 
 module.exports = (server) => {
   server.use(express.json())
   server.use(cors())
   server.use(logger('dev'))
   server.use(helmet())
+  server.use(limiter)
 }
