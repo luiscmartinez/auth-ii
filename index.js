0474027266754497e2587b683e591c198958db4f
luiscmartinez <mrluismartinezzz@gmail.com>
2018-12-04 15:49:23 -0800

    debuggin port

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 require('dotenv').config()
 const server = require('express')()
 const { sequelize, Users, Google_users } = require('./components')
-const port = process.env.PORT || 8000
-
+const port = process.env.PORT || 5000
+console.log(port)
+server.set('port', port)
 require('./middlewares')(server) // run thru middleware
 
 require('./components/user/')(server) // run thru components
 require('./passport')(server)
+
 server.get('/', (req, res) => {
   res.status(200).json({ sanity: 'check' })
 })
 require('./errorhandling')(server) // run thru errorhandlers
-
-server.set('port', port)
 
 sequelize.sync().then(() => {
   server.listen(port, () => console.log('\n=== API RUNNING... ===\n'))
