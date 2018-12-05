c27ed646faf458e823d56d3cedc90b9dd6c9fa6f
luiscmartinez <mrluismartinezzz@gmail.com>
2018-12-04 19:15:13 -0800

    changed handler function

--- components/google_user/google_userRoutes.js
+++ components/google_user/google_userRoutes.js
@@ -10,9 +10,20 @@ router.get(
 router.get(
   '/callback',
   passport.authenticate('google', {
-    failureRedirect: 'https://damp-peak-67680.herokuapp.com'
+    failureRedirect: '/'
   }),
-  controller.redirect
+  (err, req, res, next) => {
+    // custom error handler to catch any errors, such as TokenError
+    if (err.name === 'TokenError') {
+      res.redirect('/auth/google') // redirect them back to the login page
+    } else {
+      res.redirect('/auth/google')
+    }
+  },
+  (req, res) => {
+    // On success, redirect back to '/'
+    res.redirect('/')
+  }
 )
 
 module.exports = router
